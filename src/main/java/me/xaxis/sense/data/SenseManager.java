e19df104ef1028586cad7b35103355cccf1c01e6
XaxisPlayz <66885524+XaxisPlayz@users.noreply.github.com>
2022-10-09 16:09:08 -0700

    changes

---
+++ src/main/java/me/xaxis/sense/data/SenseManager.java
@@ -0,0 +1,13 @@
+package me.xaxis.sense.data;
+
+import me.xaxis.sense.Sense;
+
+public class SenseManager {
+
+    private final Sense plugin;
+
+    public SenseManager(Sense plugin){
+        this.plugin = plugin;
+    }
+
+}
